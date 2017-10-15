87457600ae4c271af95fe33476277953dcf8bfe0
gcaetano <gcaetano@Giulianos-MBP.home>
2017-10-15 23:35:17 +0100

    login

--- public/cultures.js
+++ public/cultures.js
@@ -1,11 +1,11 @@
 var express = require('express'),
     router = express.Router(),
     logger = require('../../lib/utillity/logger'),
-    BL_Users = require('../../lib/business/users').BL_Users,
+    BL_Cultures = require('../../lib/business/cultures').BL_Cultures,
     Helper = require('../../lib/utillity/helper').Helper;
 
 
- const  resource = 'users';
+ const  resource = 'cultures';
 
 //RET Representation State Transfer
 
@@ -16,11 +16,11 @@ router.get('/', (req, res, next) => {
     var options = req.query ? Helper.getReadOptions(req.query): {}
     logger.info("ROU [%s] [GET] /%s", req.sessionID, resource);
 
-    BL_Users.read(req.sessionID, options, (err, data) => {
+    BL_Cultures.read(req.sessionID, options, (err, data) => {
         var result = {success: !err, data: data || []};
         res.status(200).send(result);
         res.end();
-    })
+    });
 });
 
 /**
@@ -29,7 +29,7 @@ router.get('/', (req, res, next) => {
 router.get('/:id',(req, res, next) => {
     logger.info("ROU | [GET] /%s/id params %j", resource, req.params);
     if(req.params && req.params.id){
-        BL_Users.readOne(req, req.params.id, function (err, data) {
+        BL_Cultures.readOne(req.params.id, function (err, data) {
             var result = {success: !err, data: data || []};
             res.status(200).send(result);
             res.end();
@@ -44,13 +44,13 @@ router.get('/:id',(req, res, next) => {
 router.post('/',(req, res, next) => {
     logger.info("ROU | [POST] /%s %j", resource, req.body);
     if(req.body && req.body !== {}){
-        BL_Users.create(req, req.body, (err, insert) => {
+        BL_Cultures.create(req.body, req.session.user, (err, insert) => {
             var response = {success: !err, message: err ? err.errmsg : insert}
             res.status(200).send(response);
             res.end();
         });
     } else {
-        var message = 'bady seems empty or invalid';
+        var message = 'body seems empty or invalid';
         logger.error("ROU | [POST] /%s body %j | %s", resource, req.body, message);        
         res.status(200).send({success: false, message: message});
         res.end();
@@ -67,7 +67,7 @@ router.put('/:id', function (req, res) {
     // res.end();     
     
     if(req.body){
-        BL_Users.update(req, req.params.id, req.body, function (err, update) {
+        BL_Cultures.update(req.params.id, req.body, req.session.user, function (err, update) {
             var result = {success: !err, message: update || []};
             res.status(200).send(result);
             res.end();        
@@ -81,7 +81,7 @@ router.put('/:id', function (req, res) {
 router.delete('/:id', function (req, res) {
     logger.info("ROU | [DELETE] /%s/id %j", resource, req.params);
     if(req.params && req.params.id){
-        BL_Users.delete(req, req.params.id, function (err, remove) {
+        BL_Cultures.delete(req.params.id, function (err, remove) {
             var result = {success: !err, message: remove || []};
             res.status(200).send(result);
             res.end();        
